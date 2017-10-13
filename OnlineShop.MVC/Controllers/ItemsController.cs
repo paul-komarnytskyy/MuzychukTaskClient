aefc9862e63282ba0152c0ab0dd79a99615d5b20
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-10-13 10:59:16 +0300

    implementing base for items controller and home layout

--- OnlineShop.MVC/Controllers/ItemsController.cs
+++ OnlineShop.MVC/Controllers/ItemsController.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 
-namespace Web.MVC.Controllers
+namespace OnlineShop.MVC.Controllers
 {
-    public class HomeController : Controller
+    public class ItemsController : Controller
     {
+        // GET: Items
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Item(int ID)
         {
             var items = new List<Core.Model.DTO.Item>();
             items.Add(new Core.Model.DTO.Item() { Name = "Lenovo", Price = 1235.00, ID = 0 });
@@ -18,21 +21,7 @@ namespace Web.MVC.Controllers
             items.Add(new Core.Model.DTO.Item() { Name = "Lenovo4", Price = 1235.00, ID = 4 });
             items.Add(new Core.Model.DTO.Item() { Name = "Lenovo5", Price = 1235.00, ID = 5 });
             items.Add(new Core.Model.DTO.Item() { Name = "Lenovo6", Price = 1235.00, ID = 6 });
-            return View(items);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(items[ID]);
         }
     }
 }
