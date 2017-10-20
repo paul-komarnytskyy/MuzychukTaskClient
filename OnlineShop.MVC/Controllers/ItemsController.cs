88eed4c00c43a32d4158b23d9c6937c2c6b16cc5
Paul Komarnytskyy <paul.komarnytskyy@gmail.com>
2017-10-20 11:29:32 +0300

    Implementing base for UI

--- OnlineShop.MVC/Controllers/ItemsController.cs
+++ OnlineShop.MVC/Controllers/ItemsController.cs
@@ -14,13 +14,13 @@ namespace OnlineShop.MVC.Controllers
         public ActionResult Item(int ID)
         {
             var items = new List<Core.Model.DTO.Item>();
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo", Price = 1235.00, ID = 0 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo1", Price = 1235.00, ID = 1 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo2", Price = 1235.00, ID = 2 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo3", Price = 1235.00, ID = 3 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo4", Price = 1235.00, ID = 4 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo5", Price = 1235.00, ID = 5 });
-            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo6", Price = 1235.00, ID = 6 });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo", Price = 1235.00, ID = 0, ImageURL = "~/Content/img/123.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo1", Price = 1235.00, ID = 1, ImageURL = "~/Content/img/123.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo2", Price = 1235.00, ID = 2, ImageURL = "~/Content/img/345.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo3", Price = 1235.00, ID = 3, ImageURL = "~/Content/img/123.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo4", Price = 1235.00, ID = 4, ImageURL = "~/Content/img/345.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo5", Price = 1235.00, ID = 5, ImageURL = "~/Content/img/345.png" });
+            items.Add(new Core.Model.DTO.Item() { Name = "Lenovo6", Price = 1235.00, ID = 6, ImageURL = "~/Content/img/123.png" });
             return View(items[ID]);
         }
     }
